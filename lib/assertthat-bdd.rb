4caf485bdf0d15f255df0f795bc33a6da8004dad
Glib Briia <glib.briia@gmail.com>
2021-11-22 20:54:52 +0200

    Add scenario count

--- lib/assertthat-bdd.rb
+++ lib/assertthat-bdd.rb
@@ -7,15 +7,30 @@ module AssertThatBDD
   class Features
     def self.download(accessKey: ENV['ASSERTTHAT_ACCESS_KEY'], secretKey: ENV['ASSERTTHAT_ACCESS_KEY'], projectId: nil, outputFolder: './features/', proxy: nil, mode: 'automated', jql: '', tags: '', jiraServerUrl: nil)
 		RestClient.proxy = proxy unless proxy.nil?
-		url = 'https://bdd.assertthat.app/rest/api/1/project/'+ projectId +'/features'
-		url = jiraServerUrl+"/rest/assertthat/latest/project/"+projectId+"/client/features" unless jiraServerUrl.nil?
+		url = ['https://heavy-bat-23.loca.lt/rest/api/1/project/', projectId, '/features'].map(&:to_s).join('')
+		url = [jiraServerUrl,"/rest/assertthat/latest/project/",projectId,"/client/features"].map(&:to_s).join('') unless jiraServerUrl.nil?
 		resource = RestClient::Resource.new(url, :user => accessKey, :password => secretKey, :content_type => 'application/zip')
 		begin
-			contents = resource.get(:accept => 'application/zip', params: {mode: mode, jql: jql, tags: tags})
+			resource.get(:accept => 'application/zip', params: {mode: mode, jql: jql, tags: tags}) do |response, request, result|
+			Dir.mkdir("#{outputFolder}") unless File.exists?("#{outputFolder}")
+			File.open("#{outputFolder}/features.zip", 'wb') {|f| f.write(response) }
+			features_count = 0
+			Zip::File.open("#{outputFolder}/features.zip") do |zip_file|
+			zip_file.each do |entry|
+				features_count = features_count + 1
+				File.delete("#{outputFolder}#{entry.name}") if File.exists?("#{outputFolder}#{entry.name}")
+				entry.extract("#{outputFolder}#{entry.name}")
+			end
+			if response.headers.member?('features_count'.to_sym) and response.headers.member?('scenarios_count'.to_sym) then
+				puts "*** INFO: #{response.headers['features_count'.to_sym]} features downloaded with #{response.headers['scenarios_count'.to_sym]} scenarios"
+			else
+				puts "*** INFO: #{features_count} features downloaded"
+			end
+			File.delete("#{outputFolder}/features.zip")
 	    rescue => e
 	    	 
 	  			if e.respond_to?('response') then
-		          if e.response.respond_to?('code') then
+					if e.response.respond_to?('code') then
 		            case e.response.code
 		              when 401
 		                puts '*** ERROR: Unauthorized error (401). Supplied secretKey/accessKey is invalid'
@@ -25,23 +40,13 @@ module AssertThatBDD
 		                puts '*** ERROR: Jira server error (500)'
 		            end
 		          end
-		        else
+				  else
 		        	puts '*** ERROR: Failed download features: ' + e.message
 		        end
 				return
 		end
-		Dir.mkdir("#{outputFolder}") unless File.exists?("#{outputFolder}")
-		File.open("#{outputFolder}/features.zip", 'wb') {|f| f.write(contents) }
-		features_count = 0
-		Zip::File.open("#{outputFolder}/features.zip") do |zip_file|
-		  zip_file.each do |entry|
-		  	features_count = features_count + 1
-			File.delete("#{outputFolder}#{entry.name}") if File.exists?("#{outputFolder}#{entry.name}")
-			entry.extract("#{outputFolder}#{entry.name}")
-		  end
 		end
-		puts "*** INFO: #{features_count} features downloaded"
-		File.delete("#{outputFolder}/features.zip")
+		end
 	end
   end
   
